97b8c021ba629c97f2deae6118db6be9725bcf2d
Aaron Toliver <flux.charlotte@gmail.com>
2021-11-02 15:17:44 -0400

    redeem page

--- components/MemberList.js
+++ components/MemberList.js
@@ -24,6 +24,26 @@ const MemberList = () => {
         })
     }
 
+    const ShowPoints = () => {
+        if (deed > 0) {
+            return (
+                <div>
+                    <h1 className="text-lg text-center bg-red-900 p-2 text-white my-5 rounded-md shadow-lg">
+                        Give Deeds for the week to see your points. {deed} left.
+                    </h1>
+                    {RenderCelebrations()}
+                </div>
+            )
+        }
+
+        return (
+            <div className="text-lg text-center bg-blue-900 p-2 text-white my-5 rounded-md shadow-lg">
+                {points}
+                {RenderCelebrations()}
+            </div>
+        )
+    }
+
     const giveDeed = () => {
         let count = deed;
         let press = count -1
@@ -41,6 +61,24 @@ const MemberList = () => {
         let count = triumph;
         let press = count -1
         setTriumph(press)
+    }
+
+    const giveDeedPoints = () => {
+        let score = points;
+        let press = points + 200
+        setPoints(press)
+    }
+
+    const giveFeatPoints = () => {
+        let score = points;
+        let press = points + 500
+        setPoints(press)
+    }
+
+    const giveTriumphPoints = () => {
+        let score = points;
+        let press = points + 1000
+        setPoints(press)
     }
 
 
@@ -90,8 +128,24 @@ const MemberList = () => {
 
     const renderOther = () => {
         return (
-            <div className="bg-gray-400">
-
+            <div className="bg-gray-400 my-1 flex flex-col p-2 rounded-lg shadow-lg">
+                <h1 className="text-center font-extrabold text-2xl">
+                    Lisa
+                </h1>
+                <div className="bg-green-900 text-white rounded-lg flex my-2 flex-col">
+                        Celebrating Bobbie
+                    <div className="flex justify-center gap-2">
+                        <button className="bg-white text-black uppercase m-2 p-1 rounded-md" onClick={giveDeedPoints}>
+                            deed
+                        </button>
+                        <button className="bg-white text-black uppercase m-2 p-1 rounded-md" onClick={giveFeatPoints}>
+                            feat
+                        </button>
+                        <button className="bg-white text-black uppercase m-2 p-1 rounded-md" onClick={giveTriumphPoints}>
+                            triumph
+                        </button>
+                    </div>
+                </div>    
             </div>
         )
     }
@@ -108,10 +162,11 @@ const MemberList = () => {
             <h1 className="text-2xl underline">
                 Your Colleagues
             </h1>
-            {RenderCelebrations()}
+            {ShowPoints()}
             <div className="my-1 flex flex-col bg-white p-2 rounded-lg shadow-lg">
                 {LoopMembers()}
-            </div>       
+            </div>   
+            {renderOther()}
         </div>
     )
 }
--- pages/season.js
+++ pages/season.js
@@ -1,9 +1,38 @@
-import Layout from "../components/Layout"
+import React, { useState } from "react";
+import Layout from "../components/Layout";
+import rewards from "../data/rewards";
+import members from "../data/members";
 
 const season = () => {
+    const [points, setPoints] = useState(1200)
+
+    const renderRewards = () => {
+        return rewards.map(reward => {
+            return (
+                <div key={reward.id} className="mx-2 my-5 bg-white rounded-lg shadow-lg p-2">
+                    <h1 className="text-xl uppercase font-semibold text-blue-500">
+                        {reward.name}
+                    </h1>
+                    <p className="font-medium uppercase my-3">
+                        {reward.description}
+                    </p>
+                    <p className="font-light italic my-3">
+                        $ {reward.price} value - {reward.quantity} left
+                    </p>
+                    <button className="bg-green-900 text-white p-2">
+                        Redeem
+                    </button>
+                </div>
+            )
+        })
+    }
+
     return (
         <Layout>
-            season
+            <h1 className="font-extrabold text-2xl uppercase text-center my-3">
+                you have {points} points
+            </h1>
+            {renderRewards()}
         </Layout>
     )
 }
